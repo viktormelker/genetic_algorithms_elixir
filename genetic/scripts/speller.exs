533c13941db06d2dc841d8764a348e200a89e5ec
Viktor Melker <viktormelker@gmail.com>
2023-11-17 09:13:00 +0100

    Chapter 3: Create problem abstraction

---
+++ genetic/scripts/speller.exs
@@ -0,0 +1,30 @@
+defmodule Speller do
+  @behaviour Problem
+  alias Types.Chromosome
+
+  @impl true
+  def genotype() do
+    genes =
+      Stream.repeatedly(fn -> Enum.random(?a..?z) end)
+      |> Enum.take(34)
+    %Chromosome{genes: genes, size: 34}
+  end
+
+  @impl true
+  def fitness_function(chromosome) do
+    target = "supercalifragilisticexpialidocious"
+    guess = List.to_string(chromosome.genes)
+    String.jaro_distance(target, guess)
+  end
+
+  @impl true
+  def terminate?([best | _]) do
+    best.fitness == 1
+  end
+
+end
+
+
+solution = Genetic.run(Speller)
+IO.puts("Found solution")
+IO.inspect(solution)
