5ae88315c2ef07ad15fa641f74a586cdc77b97bb
Viktor Melker <viktormelker@gmail.com>
2023-11-20 07:44:05 +0100

    Chapter 6: More crossover strategies

--- genetic/lib/toolbox/crossover.ex
+++ genetic/lib/toolbox/crossover.ex
@@ -22,4 +22,43 @@ defmodule Toolbox.Crossover do
     {c1, c2} = {head1 ++ slice1 ++ tail1, head2 ++ slice2 ++ tail2}
     {%Chromosome{genes: c1, size: p1.size}, %Chromosome{genes: c2, size: p2.size}}
   end
+
+  def single_point(p1, p2) do
+    cx_point = :rand.uniform(length(p1.genes))
+    {h1, t1} = Enum.split(p1.genes, cx_point)
+    {h2, t2} = Enum.split(p2.genes, cx_point)
+    {c1, c2} = {h1 ++ t2, h2 ++ t1}
+    {%Chromosome{p1 | genes: c1}, %Chromosome{p2 | genes: c2}}
+  end
+
+  def uniform(p1, p2, rate) do
+    {c1, c2} =
+      p1.genes
+      |> Enum.zip(p2.genes)
+      |> Enum.map(fn {x, y} ->
+        if :rand.uniform() < rate do
+          {y, x}
+        else
+          {x, y}
+        end
+      end)
+      |> Enum.unzip()
+
+    {%Chromosome{genes: c1, size: length(c1)}, %Chromosome{genes: c2, size: length(c2)}}
+  end
+
+  def whole_arithmetic(p1, p2, alpha) do
+    {c1, c2} =
+      p1.genes
+      |> Enum.zip(p2.genes)
+      |> Enum.map(fn {x, y} ->
+        {
+          x * alpha + y * (1 - alpha),
+          x * (1 - alpha) + y * alpha
+        }
+      end)
+      |> Enum.unzip()
+
+    {%Chromosome{genes: c1, size: length(c1)}, %Chromosome{genes: c2, size: length(c2)}}
+  end
 end
