1245cbbd4f17e42e3e69de0bc283bbf4fee93e67
Viktor Melker <viktormelker@gmail.com>
2024-01-05 23:05:47 +0100

    chapter 8-1: add schedule script. Fix crossover issue

--- genetic/lib/toolbox/crossover.ex
+++ genetic/lib/toolbox/crossover.ex
@@ -2,25 +2,29 @@ defmodule Toolbox.Crossover do
   alias Types.Chromosome
 
   def order_one_crossover(p1, p2) do
-    lim = Enum.count(p1.genes) - 1
+    # Order-one crossover, sometimes called “Davis order” crossover, is a crossover
+    # strategy on ordered lists or permutations. Order-one crossover is part of a
+    # unique set of crossover strategies that will preserve the integrity of a permutation
+    # solution.
+    # This wont work well for NON permuatation list as size of genes will no keep
+    # same size.
+    size = p1.size - 1
 
-    {i1, i2} =
-      [:rand.uniform(lim), :rand.uniform(lim)]
+    {from, to} =
+      [:rand.uniform(size), :rand.uniform(size)]
       |> Enum.sort()
       |> List.to_tuple()
 
-    slice1 = Enum.slice(p1.genes, i1..i2)
-    slice1_set = MapSet.new(slice1)
-    p2_contrib = Enum.reject(p2.genes, &MapSet.member?(slice1_set, &1))
-    {head1, tail1} = Enum.split(p2_contrib, i1)
+    slice1 = Enum.slice(p1.genes, from..to)
+    {head1, tail1} = (p2.genes -- slice1) |> Enum.split(from)
 
-    slice2 = Enum.slice(p2.genes, i1..i2)
-    slice2_set = MapSet.new(slice2)
-    p1_contrib = Enum.reject(p1.genes, &MapSet.member?(slice2_set, &1))
-    {head2, tail2} = Enum.split(p1_contrib, i1)
+    slice2 = Enum.slice(p2.genes, from..to)
+    {head2, tail2} = (p1.genes -- slice2) |> Enum.split(from)
 
-    {c1, c2} = {head1 ++ slice1 ++ tail1, head2 ++ slice2 ++ tail2}
-    {%Chromosome{genes: c1, size: p1.size}, %Chromosome{genes: c2, size: p2.size}}
+    {
+      %Chromosome{p1 | genes: head1 ++ slice1 ++ tail1, size: p1.size},
+      %Chromosome{p2 | genes: head2 ++ slice2 ++ tail2, size: p1.size}
+    }
   end
 
   def single_point(p1, p2) do
