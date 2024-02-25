c7e0bf1e93f6d7a63e6d7e8355a41fc3351f3451
Viktor Melker <viktormelker@gmail.com>
2024-02-25 21:03:42 +0100

    chapter 9-1: add statistics logging

--- genetic/scripts/tiger_simulation.exs
+++ genetic/scripts/tiger_simulation.exs
@@ -24,13 +24,32 @@ defmodule TigerSimulation do
 
   @impl true
   def terminate?(_population, generation), do: generation == 1000
+
+  def average_tiger(population) do
+    genes = Enum.map(population, & &1.genes)
+    fitnesses = Enum.map(population, & &1.fitness)
+    ages = Enum.map(population, & &1.age)
+    num_tigers = length(population)
+
+    avg_fitness = Enum.sum(fitnesses) / num_tigers
+    avg_age = Enum.sum(ages) / num_tigers
+
+    avg_genes =
+      genes
+      |> Enum.zip()
+      |> Enum.map(&Tuple.to_list/1)
+      |> Enum.map(&(Enum.sum(&1) / num_tigers))
+
+    %Chromosome{genes: avg_genes, fitness: avg_fitness, age: avg_age}
+  end
 end
 
 solution =
   Genetic.run(TigerSimulation,
     population_size: 20,
-    selection_rate: 0.8,
-    mutation_rate: 0.1
+    selection_rate: 0.9,
+    mutation_rate: 0.1,
+    statistics: %{average_tiger: &TigerSimulation.average_tiger/1}
   )
 
 IO.puts("Found solution")
@@ -40,8 +59,6 @@ IO.inspect(solution)
 {_, fivehundred_gen_stats} = Utilities.Statistics.lookup(500)
 {_, onethousand_gen_stats} = Utilities.Statistics.lookup(1000)
 
-IO.write("""
-0th: #{zero_gen_stats.mean_fitness}
-500th: #{fivehundred_gen_stats.mean_fitness}
-1000th: #{onethousand_gen_stats.mean_fitness}
-""")
+dbg(zero_gen_stats)
+dbg(fivehundred_gen_stats)
+dbg(onethousand_gen_stats)
